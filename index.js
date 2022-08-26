5216922756813f107d0f149796304f71394b1341
ezra.kipchumba@student.moringaschool.com <ezra.kipchumba@student.moringaschool.com>
2022-08-26 16:19:43 +0300

    complete

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
+function reverseString(word) {
+  // create an array from the input string
+  const wordArray = word.split("");
+  // reverse the array
+  const reversedWordArray = wordArray.reverse();
+  // create a string from the reversed array
+  const reversedWord = reversedWordArray.join("");
+  // return the reversed string
+  return reversedWord;
+}
 function isPalindrome(word) {
   // Write your algorithm here
+    // reverse the input string
+    const reversedWord = reverseString(word);
+    // compare the reversed string to the input
+    return word === reversedWord;
 }
 
 /* 
   Add your pseudocode here
+  reverse the input string
+
+if the reversed string is the same as the input
+  return true
+else
+  return false
 */
 
 /*
   Add written explanation of your solution here
+  create an array from the input string
+reverse the array
+create a string from the reversed array
+return the reversed string
+
 */
 
 // You can run `node index.js` to view these console logs
